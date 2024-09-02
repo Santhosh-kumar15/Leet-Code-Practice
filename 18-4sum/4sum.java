51dd0a506bbdf878c12eb2896d9855f214b2a9e5
Santhoshkumar J V <santhoshkumarjv1512@gmail.com>
2024-09-02 15:38:50 +0530

    Time: 19 ms (43.76%) | Memory: 44.1 MB (45.97%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,56 +1,44 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        long sum;
         Arrays.sort(nums);
-        int len = nums.length;
-        for(int i=0;i<len-3;i++)
+        int n = nums.length;
+        long sum;
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<n-3;i++)
         {
             if(i>0 && nums[i] == nums[i-1])
                 continue;
-            for(int j=i+1;j<len-2;j++)
+            for(int j=i+1;j<n-2;j++)
             {
                 if(j>i+1 && nums[j] == nums[j-1])
                     continue;
                 int left = j+1;
-                int right = len-1;
+                int right = n-1;
                 while(left<right)
                 {
-                    sum =(long) nums[left] + nums[right] + nums[j] + nums[i];
-                    if(sum == target)
+                    sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum==target)
                     {
-                        ArrayList<Integer> pairs = new ArrayList<>();
+                        ArrayList<Integer> pairs = new ArrayList();
                         pairs.add(nums[i]);
                         pairs.add(nums[j]);
                         pairs.add(nums[left]);
                         pairs.add(nums[right]);
                         Collections.sort(pairs);
                         result.add(pairs);
-                        left++;
                         right--;
-
+                        left++;
                         while(left<right && nums[right] == nums[right+1])
-                        {
                             right--;
-                        }
-
                         while(left<right && nums[left] == nums[left-1])
-                        {
                             left++;
-                        }
                     }
-                    else if(sum<target)
-                    {
-                        left++;
-                    }
-                    else
-                    {
+                    else if(sum>target)
                         right--;
-                    }
-
+                    else
+                        left++;
                 }
             }
-            
         }
         return result;
     }
