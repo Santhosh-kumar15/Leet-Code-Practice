b5f56065d6dbcf86d2d45f52a5a31d6a07233d1c
Santhoshkumar J V <santhoshkumarjv1512@gmail.com>
2024-12-12 19:54:39 +0530

    Time: 8 ms (9.65%) | Memory: 42.4 MB (5.85%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,15 +1,13 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 1) return strs[0];
-
-        String ans = "";
-
         Arrays.sort(strs);
+        String ans = "";
         String s1 = strs[0];
         String s2 = strs[strs.length - 1];
 
         int x = 0;
         int y = 0;
+
         while(x < s1.length() && y < s2.length())
         {
             if(s1.charAt(x) == s2.charAt(y))
@@ -23,7 +21,8 @@ class Solution {
                 break;
             }
         }
-        if(ans.length() == 0) return "";
+        if(ans.length() <= 0)
+            return "";
         return ans;
     }
 }
