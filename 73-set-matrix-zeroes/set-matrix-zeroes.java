369d4d50e4ae0467a5063fa6e635e641b1726ec6
Santhoshkumar J V <santhoshkumarjv1512@gmail.com>
2024-12-20 08:59:34 +0530

    Time: 1 ms (75.63%) | Memory: 45.8 MB (29.36%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,9 +1,10 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        // col --> matrix[0][...]
+        // row --> matrix[...][0]
         int n = matrix.length;
         int m = matrix[0].length;
-        int[] row = new int[n];
-        int[] col = new int[m];
+        int col0 = 1;
 
         for(int i = 0; i < n; i++)
         {
@@ -11,21 +12,43 @@ class Solution {
             {
                 if(matrix[i][j] == 0)
                 {
-                    row[i] = 1;
-                    col[j] = 1;
+                    // mark i'th row
+                    matrix[i][0] = 0;
+                    // mark j'th col
+                    if(j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
                 }
             }
         }
 
-        for(int i = 0; i < n; i++)
+        for(int i = 1; i < n; i++)
         {
-            for(int j = 0; j < m; j++)
+            for(int j = 1; j < m; j++)
             {
-                if(row[i] == 1 || col[j] == 1)
+                // check for col & row
+                if(matrix[i][j] != 0)
                 {
-                    matrix[i][j] = 0;
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                        matrix[i][j] = 0;
                 }
             }
         }
+        if(matrix[0][0] == 0)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        } 
+
     }
 }
